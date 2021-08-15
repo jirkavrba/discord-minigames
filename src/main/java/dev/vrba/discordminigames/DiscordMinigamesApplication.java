14b8fc6c5df8267159852ee480ef9bd9b8ed03b9
Jiří Vrba <jiri@vrba.dev>
2021-08-15 09:45:34 +0200

    Initial commit, fresh Spring boot application

---
+++ src/main/java/dev/vrba/discordminigames/DiscordMinigamesApplication.java
@@ -0,0 +1,13 @@
+package dev.vrba.discordminigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordMinigamesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiscordMinigamesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/vrba/discordminigames/DiscordMinigamesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.vrba.discordminigames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordMinigamesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
