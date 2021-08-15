fdd99bc60991b326e9a993c6bb6391a37554eacb
Jiří Vrba <jiri@vrba.dev>
2021-08-15 15:44:06 +0200

    Start the discord service on application startup

--- src/main/java/dev/vrba/discordminigames/DiscordMinigamesApplication.java
+++ src/main/java/dev/vrba/discordminigames/DiscordMinigamesApplication.java
@@ -1,13 +1,25 @@
 package dev.vrba.discordminigames;
 
+import dev.vrba.discordminigames.services.DiscordService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DiscordMinigamesApplication {
+public class DiscordMinigamesApplication implements CommandLineRunner {
+
+    private final DiscordService service;
+
+    public DiscordMinigamesApplication(DiscordService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        this.service.start();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DiscordMinigamesApplication.class, args);
     }
-
 }
