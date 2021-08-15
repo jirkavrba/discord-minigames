f6cd988292bc30b6894444c3f100a507bced101c
Jiří Vrba <jiri@vrba.dev>
2021-08-15 16:15:40 +0200

    Add discord colors constants

---
+++ src/main/java/dev/vrba/discordminigames/utilities/DiscordColors.java
@@ -0,0 +1,16 @@
+package dev.vrba.discordminigames.utilities;
+
+import java.awt.Color;
+
+public class DiscordColors {
+
+    public static final Color Primary = new Color(0x5865F2);
+
+    public static final Color Green = new Color(0x57F287);
+
+    public static final Color Yellow = new Color(0xFEE75C);
+
+    public static final Color Fuchsia = new Color(0xEB459E);
+
+    public static final Color Red = new Color(0xED4245);
+}
