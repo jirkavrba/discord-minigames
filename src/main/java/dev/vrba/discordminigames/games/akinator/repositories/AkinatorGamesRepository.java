669e000034a8d2afef0bd7d942bb49873ae8961e
Jiří Vrba <jiri@vrba.dev>
2021-08-15 20:31:27 +0200

    Formatting

--- src/main/java/dev/vrba/discordminigames/games/akinator/repositories/AkinatorGamesRepository.java
+++ src/main/java/dev/vrba/discordminigames/games/akinator/repositories/AkinatorGamesRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface AkinatorGamesRepository extends CrudRepository<AkinatorGame, UUID > {
+public interface AkinatorGamesRepository extends CrudRepository<AkinatorGame, UUID> {
 }
