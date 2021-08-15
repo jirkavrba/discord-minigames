20e5a2feb4167cab2d4715244cae72d52927a35a
Jiří Vrba <jiri@vrba.dev>
2021-08-15 20:35:54 +0200

    Add method for finding akinator games by message and user

--- src/main/java/dev/vrba/discordminigames/games/akinator/repositories/AkinatorGamesRepository.java
+++ src/main/java/dev/vrba/discordminigames/games/akinator/repositories/AkinatorGamesRepository.java
@@ -4,8 +4,13 @@ import dev.vrba.discordminigames.games.akinator.entities.AkinatorGame;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface AkinatorGamesRepository extends CrudRepository<AkinatorGame, UUID> {
+
+    Optional<AkinatorGame> findByMessageAndUser(long message, long user);
+
 }
